d32a28bec7aeb6143484cc3e344f82189bbf0b0a
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-03-03 04:53:21 +0300

    fixed 5th task

--- Homework1/Homework1/Program.fs
+++ Homework1/Homework1/Program.fs
@@ -4,7 +4,7 @@
         else factorialAcc (acc * x) (x - 1)
     factorialAcc 1 x
     
-let fib n =
+let fibonacci n =
     let rec fibCounter x y i n =
         if n = 0 then 0
         elif n = 1 then 1
@@ -13,23 +13,22 @@ let fib n =
     fibCounter 0 1 2 n
     
 let reverse list =
-    let rec reverse_acc list acc = 
+    let rec reverseAcc list acc = 
         match list with
-        | h :: t -> reverse_acc t (h::acc)
+        | h :: t -> reverseAcc t (h :: acc)
         | [] -> acc
-    reverse_acc list []
+    reverseAcc list []
     
 let generatePowers n m =
     let rec generatePowersAcc m pow acc = 
-        if m = -1.0 then acc
-        else generatePowersAcc (m - 1.0) (pow * 2.0) (acc @ [pow])
+        if m = -1 then acc
+        else generatePowersAcc (m - 1) (pow * 2.0) (acc @ [pow])
     generatePowersAcc m (2.0 ** n) []
     
 let findElement ls value = 
     let rec numPosition ls i = 
         match ls with
-        | h :: t when h = value -> i
-        | h :: t when h <> value -> numPosition t (i + 1)
-        | [] -> -1
+        | h :: t -> if h = value then Some(i) else numPosition t (i + 1)
+        | [] -> None
     numPosition ls 0
 
