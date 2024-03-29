38c8deba3e40c44e0c9edf25d1965f5e7b9a2ede
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-03-29 23:35:11 +0300

    added first test

--- ConsoleApp1/Homework3.Tests/LambdaTest.fs
+++ ConsoleApp1/Homework3.Tests/LambdaTest.fs
@@ -1,1 +1,10 @@
 ﻿module Homework3.Tests.LambdaTest
+open Homework3.Lambda
+open NUnit.Framework
+open FsUnit
+[<Test>]
+let ``(lambda(y).y x) z -> z x``() = 
+    let inputExpr = Application(Abstraction("y", Application(Variable "y", Variable "x")), Variable "z")
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Application(Variable "z", Variable "x")
+    Assert.AreEqual(correctExpr, reducedExpr )
