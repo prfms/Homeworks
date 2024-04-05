c1d91d623625891136405fc72a1670536df2c542
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-04-05 23:00:45 +0300

    added tests for lambda

--- ConsoleApp1/Homework3.Tests/LambdaTest.fs
+++ ConsoleApp1/Homework3.Tests/LambdaTest.fs
@@ -2,9 +2,36 @@
 open Homework3.Lambda
 open NUnit.Framework
 open FsUnit
+
+[<Test>]
+let ``Free variable of lambda(x).x y is "y"``() =
+    let inputExpr = Abstraction("x", Application(Variable "x", Variable "y"))
+    let freeVarExpr = freeVar inputExpr
+    let correctFreeVar = ["y"]
+    Assert.AreEqual(correctFreeVar, freeVarExpr)
+[<Test>]
+let ``Bound variable of lambda(x).x y is "x"``() =
+    let inputExpr = Abstraction("x", Application(Variable "x", Variable "y"))
+    let boundVarExpr = boundVar inputExpr
+    let correctBoundVar = ["x"]
+    Assert.AreEqual(correctBoundVar, boundVarExpr)
+
 [<Test>]
 let ``(lambda(y).y x) z -> z x``() = 
     let inputExpr = Application(Abstraction("y", Application(Variable "y", Variable "x")), Variable "z")
     let reducedExpr = betaReduce inputExpr
     let correctExpr = Application(Variable "z", Variable "x")
-    Assert.AreEqual(correctExpr, reducedExpr )
+    Assert.AreEqual(correctExpr, reducedExpr)
+[<Test>]
+let ``(lambda(y).y x) (lambda(z).z) -> lambda(z).z x``() = 
+    let inputExpr = Application(Abstraction("y", Application(Variable "y", Variable "x")), Abstraction("z", Variable "z"))
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Abstraction("z", Application(Variable "z", Variable "x"))
+    Assert.AreEqual(correctExpr, reducedExpr)
+    
+[<Test>]
+let ``(lambda(x). x y) y -> *y y``() =
+    let inputExpr = Application(Abstraction("x",Application(Variable "x", Variable "y")), Variable "y")
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Application(Variable "*y", Variable "y")
+    Assert.AreEqual(correctExpr, reducedExpr)
--- ConsoleApp1/Homework3/Lambda.fs
+++ ConsoleApp1/Homework3/Lambda.fs
@@ -5,28 +5,47 @@ type LambdaExpression =
     | Abstraction of string * LambdaExpression
     | Application of LambdaExpression * LambdaExpression
 
+let boundVar expr =
+    let rec boundVarAcc expr acc =
+        match expr with
+        | Variable(x) -> acc
+        | Application(func, arg) -> boundVarAcc func acc @ boundVarAcc arg acc
+        | Abstraction(param, body) -> boundVarAcc body (param :: acc)
+    boundVarAcc expr [] |> List.distinct    
+let freeVar expr =
+    let rec freeVarAcc expr acc =
+        match expr with
+        | Variable(x) -> (x::acc)
+        | Application(func, arg) -> freeVarAcc func acc @ freeVarAcc arg acc
+        | Abstraction(param, body) -> freeVarAcc body acc |> List.filter (fun x -> x <> param)
+    freeVarAcc expr [] |> List.distinct
+let rec alphaConvert var newVar expression =
+    match expression with
+    | Variable(x) when x = var -> Variable(newVar)
+    | Abstraction(param, body) when param = var -> Abstraction(newVar, alphaConvert var newVar body)
+    | Abstraction(param, body) -> Abstraction(param, alphaConvert var newVar body)
+    | Application(func, arg) -> Application(alphaConvert var newVar func, alphaConvert var newVar arg)
+    | _ -> expression
 let rec substitute expr varName subExpr =
     match expr with
     | Variable name -> if name = varName then subExpr else expr
-    | Abstraction (name, body) -> 
-        if name = varName then expr 
-        else 
-            Abstraction(name, substitute body varName subExpr)
     | Application (func, arg) -> 
-        Application(substitute func varName subExpr, substitute arg varName subExpr)
-
+        Application (substitute func varName subExpr, substitute arg varName subExpr)
+    | Abstraction (param, body) when param = varName -> expr
+    | Abstraction (param, body) when not (freeVar body |> List.exists(fun x -> x = varName)) || not(freeVar subExpr |> List.exists(fun x -> x = param)) ->
+        Abstraction(param, substitute body varName subExpr)
+    | Abstraction(param, body) ->
+        let newParam = "*" + param
+        Abstraction(newParam, substitute (alphaConvert param newParam body) varName subExpr)    
+       
 let rec betaReduce expr =
     match expr with
     | Application (Abstraction (param, body), arg) -> 
-        let renamedBody = substitute body param arg
-        betaReduce renamedBody
+        betaReduce (substitute body param arg)
     | Application (func, arg) -> 
-        let reducedFunc = betaReduce func
-        let reducedArg = betaReduce arg
-        Application(reducedFunc, reducedArg)
+        Application(betaReduce func, betaReduce arg)
     | Abstraction (param, body) -> 
-        let reducedBody = betaReduce body
-        Abstraction(param, reducedBody)
+        Abstraction(param, betaReduce body)
     | _ -> expr
 
 let rec printLambda (expr: LambdaExpression): string =
