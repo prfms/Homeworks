83ea8b666134bae5a9d5613841f055b1ec2ae3d9
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-05-17 17:01:39 +0300

    added supermap, filter for trees and blocking queue

---
+++ test2/test2.Test/tasks.Tests.fs
@@ -0,0 +1,80 @@
+﻿module tasks.Tests.fs
+open System.Threading
+open System.Threading.Tasks
+open NUnit.Framework
+open FsUnit
+open tasks
+
+[<Test>]
+let ``Super map of 2 functions should return correct list``() =
+    superMap [1.0; 2.0; 3.0] (fun x -> [sin x; cos x]) |> should equal [sin 1.0; cos 1.0; sin 2.0; cos 2.0; sin 3.0; cos 3.0]
+[<Test>]
+let ``Super map of 1 function should return correct list``() =
+    superMap [1.0; 2.0; 3.0] (fun x -> [x + 1.0]) |> should equal [2.0; 3.0; 4.0]
+    
+[<Test>]
+let ``Super map of empty function should return empty list``() =
+    superMap [1.0; 2.0; 3.0] (fun _ -> []) |> should equal []
+    
+[<Test>]
+let ``Super map of string list should return correct list``() =
+    superMap ["aa"; "bb"] (fun x -> [x + "d"; x + "e"]) |> should equal ["aad"; "aae"; "bbd"; "bbe"]
+    
+[<Test>]
+let ``Filter should return correct value for not empty tree``() =
+    let tree =
+        Node(5,
+            Node(3,
+                Node(1, Empty, Empty),
+                Node(4, Empty, Empty)),
+            Node(8,
+                Node(6, Empty, Empty),
+                Node(10, Empty, Empty)))
+
+    let isEven x = x % 2 = 0
+
+    filterTree isEven tree |> should equal [4; 8; 6; 10]
+    
+[<Test>]
+let ``Filter for an empty tree should return empty list``() =
+    let tree = Empty
+    let isEven x = x % 2 = 0
+    
+    filterTree isEven tree |> should equal []
+    
+[<Test>]
+let ``Adding and deleting the item to the queue should return an empty queue``() =
+    let blockingQueue = BlockingQueue<int>()
+    let customer = 
+        async {
+                blockingQueue.Enqueue(1)
+                blockingQueue.Dequeue() |> ignore
+                do! Async.Sleep(500)
+            }
+
+    Async.Start customer
+
+    Thread.Sleep(1000)
+    
+    Assert.AreEqual(blockingQueue.IsEmpty, true)
+    
+[<Test>]
+let ``Dequeue from empty queue should wait until enqueue``() =
+    let queue = BlockingQueue<int>()
+    let itemToEnqueue = 1
+    let mutable itemFromQueue = 0
+    let dequeueTask = Task.Run(fun () ->
+        itemFromQueue <- queue.Dequeue()
+    )
+    
+    Thread.Sleep(1000)
+    
+    Assert.AreEqual(0, itemFromQueue)
+   
+    let enqueueTask = Task.Run(fun () ->
+        queue.Enqueue(itemToEnqueue)
+    )
+
+    Task.WaitAll([| dequeueTask; enqueueTask |])
+
+    Assert.AreEqual(itemToEnqueue, itemFromQueue)
---
+++ test2/test2/tasks.fs
@@ -0,0 +1,67 @@
+﻿module tasks
+open System.Collections.Generic
+open System.Threading
+
+/// <summary>
+/// Method to apply function to each element of the list. 
+/// </summary>
+/// <param name="ls">The list.</param>
+/// <param name="func">The function.</param>
+let superMap ls func =
+    ls |> List.collect func
+    
+/// <summary>
+/// Discriminated Union to implement the binary tree
+/// </summary>
+type BinaryTree<'a> =
+    | Empty
+    | Node of 'a * BinaryTree<'a> * BinaryTree<'a>
+    
+/// <summary>
+/// Method to get elements of the bin tree that satisfy the predicate.
+/// </summary>
+/// <param name="predicate">The filter function.</param>
+/// <param name="tree">The binary tree.</param>
+let filterTree predicate tree =
+    let rec filterTreeHelper predicate tree =
+        match tree with
+        | Empty -> []
+        | Node (value, left, right) ->
+            let leftFiltered = filterTreeHelper predicate left
+            let rightFiltered = filterTreeHelper predicate right
+            if predicate value then
+                value :: (leftFiltered @ rightFiltered)
+            else
+                leftFiltered @ rightFiltered
+    filterTreeHelper predicate tree
+
+/// <summary>
+/// Class representing blocking queue
+/// </summary>
+type BlockingQueue<'T>() =
+    let queue = Queue<'T>()
+    let lockObj = obj()
+    
+    /// <summary>
+    /// Method to add item to queue
+    /// </summary>
+    member this.Enqueue(item: 'T) =
+        lock lockObj (fun () ->
+            queue.Enqueue(item)
+            Monitor.Pulse(lockObj))  
+    
+    /// <summary>
+    /// Method to delete the item from the queue.
+    /// </summary>
+    member this.Dequeue() : 'T =
+        lock lockObj (fun () ->
+            while queue.Count = 0 do
+                Monitor.Wait(lockObj)  
+            queue.Dequeue())
+    
+    /// <summary>
+    /// Method to check if the queue is empty.
+    /// </summary>
+    member this.IsEmpty =
+        lock lockObj (fun () ->
+            queue.Count = 0)
