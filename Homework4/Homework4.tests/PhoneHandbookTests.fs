e184a1599904a6a031356c5e67099e76b2a2d7a0
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-05-18 18:24:03 +0300

    added some and none, added tests for dublicates

--- Homework4/Homework4.tests/PhoneHandbookTests.fs
+++ Homework4/Homework4.tests/PhoneHandbookTests.fs
@@ -1,8 +1,10 @@
 ﻿module PhoneHandbookTest.fs
-open System.IO
+open PhoneHandbook
 open NUnit.Framework
 open FsUnit
     
+exception FileError of string
+
 [<Test>]
 let ``Add record to an empty handbook and find it by name and phone should return this record's data.``() =
     let record =  { Name = "Anna"; Phone = "8928" }
@@ -13,10 +15,23 @@ let ``Add record to an empty handbook and find it by name and phone should retur
     Assert.AreEqual(findName[0], "Anna")
     
 [<Test>]
-let ``Write handbook to file and then read it back should return the same data``() =
-    let fileName = "C://Users//nasty//RiderProjects//Homeworks//Homework4//Homework4//test.txt"
-    let originalHandbook = [{ Name = "Anna"; Phone = "8928" }; { Name = "Nick"; Phone = "000" }]
-    writeToFile originalHandbook fileName
-    let readHandbook = readFromFile fileName
-    Assert.AreEqual(originalHandbook, readHandbook)
+let ``Add records with same names and different phones and find them by names``() =
+    let record1 =  { Name = "Anna"; Phone = "8928" }
+    let record2 =  { Name = "Anna"; Phone = "0000" }
+    let handbook = addRecord [] record1
+    let handbook = addRecord handbook record2
+    let findPhone = findPhoneByName "Anna" handbook
+    Assert.AreEqual(findPhone[0], "0000")
+    Assert.AreEqual(findPhone[1], "8928")
     
+[<Test>]
+let ``Write handbook to file and then read it back should return the same data``() =
+    let fileName = "C:\\Users\\nasty\\RiderProjects\\Homeworks\\Homework4\\Homework4\\test.txt"
+    let originalHandbook = [{ Name = "Anna"; Phone = "8928" }; { Name = "Nick"; Phone = "000" }]
+    match writeToFile originalHandbook fileName with
+    | None -> raise (FileError("Writing to file error."))
+    | Some () ->
+        match readFromFile fileName with
+        | None -> raise (FileError("Reading from file error."))
+        | Some (readHandbook: PhoneRecord list) -> originalHandbook |> should equal readHandbook
+            
--- Homework4/Homework4/PhoneHandbook.fs
+++ Homework4/Homework4/PhoneHandbook.fs
@@ -8,40 +8,36 @@ type PhoneRecord =
     }
     
 let addRecord handbook record =
-    handbook @ [record]
+    record :: handbook
 
 let findPhoneByName name handbook =
     handbook |> List.filter (fun x -> x.Name = name) |> List.map _.Phone
     
 let findNameByPhone phone handbook =
     handbook |> List.filter (fun x -> x.Phone = phone) |> List.map _.Name
-
-let printRecord record =
-    printfn $"Name: %s{record.Name}, Phone: %s{record.Phone}"
     
 let printHandbook handbook =
-    printfn "Dictionary includes:\n"
-    handbook |> List.map printRecord
+    handbook |> Seq.map (fun record -> $"Name: %s{record.Name}, Phone: %s{record.Phone}")
+        |> String.concat "\n"
 
 let writeToFile handbook fileName =
     try
         let records = handbook |> List.map (fun record -> $"%s{record.Name} %s{record.Phone}")
         File.WriteAllLines(fileName, records)
+        Some ()
     with
-    | :? IOException as ex -> printfn $"Error writing to file: %s{ex.Message}"
-
+    | :? IOException -> None
+     
 let readFromFile fileName =
     try
-        File.ReadLines(fileName)
-        |> Seq.fold (fun handbook record -> 
-            let parts = record.Split [|' '|]
-            match parts with
-            | [| name; phone |] -> addRecord handbook { Name = name; Phone = phone }
-            | _ -> failwith "Invalid record format"
-        ) []
+        let handbook =
+            File.ReadLines(fileName)
+            |> Seq.fold (fun handbook record -> 
+                let parts = record.Split [|' '|]
+                match parts with
+                | [| name; phone |] -> addRecord handbook { Name = name; Phone = phone }
+                | _ -> failwith "Invalid record format"
+            ) []
+        Some handbook
     with
-    | :? IOException as ex -> 
-        printfn $"Error reading from file: %s{ex.Message}"
-        []   
-    
-    
+    | :? IOException -> None
