41147b6702004d7e083fd17ec6d34efca353d9f3
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-05-30 22:46:03 +0300

    added round workflow

---
+++ Homework6/Homework6/RoundWorkflow.fs
@@ -0,0 +1,9 @@
+﻿module RoundWorkflow
+
+type RoundBuilder( precision : int) =
+    member this.Bind(x : float, f) =
+        let roundedValue = System.Math.Round(x, precision)
+        f roundedValue
+    member this.Return(x : float) =
+        let roundedValue = System.Math.Round(x, precision)
+        roundedValue
