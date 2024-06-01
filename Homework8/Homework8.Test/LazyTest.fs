85bd28a357fcabebf1621ae64d89417747d600d8
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-06-01 05:59:10 +0300

    added lazy and tests

--- Homework8/Homework8.Test/LazyTest.fs
+++ Homework8/Homework8.Test/LazyTest.fs
@@ -1,58 +1,77 @@
 ﻿module LazyTest
 open NUnit.Framework
 open FsUnit
-open System.Threading
 open Lazy
-let testSupplier () = 
-    1
 
-let testSupplierWithCounter (computationCount: byref<int>) () =
-    computationCount <- computationCount + 1
-    1
+let mutable counter = 0
 
-let singleThreadedLazySupplier supplier = SingleThreadedLazy<int>(supplier) :> ILazy<int>
-let multiThreadedLazySupplier supplier = MultiThreadedLazy<int>(supplier) :> ILazy<int>
-let lockFreeLazySupplier supplier = LockFreeLazy<int>(supplier) :> ILazy<int>
+let AllLazyCases =
+    [ (fun f -> SingleThreadedLazy f :> ILazy<obj>)
+      (fun f -> MultiThreadedLazy f :> ILazy<obj>)
+      (fun f -> LockFreeLazy f :> ILazy<obj>) ]
+    |> List.map (fun f -> TestCaseData(f))
 
-let lazyImplementations =
-    [
-        TestCaseData(singleThreadedLazySupplier).SetName("SingleThreadedLazy")
-        TestCaseData(multiThreadedLazySupplier).SetName("MultiThreadedLazy")
-        TestCaseData(lockFreeLazySupplier).SetName("LockFreeLazy")
-    ]
+let MultiThreadedLazyCases =
+    [ (fun f -> MultiThreadedLazy f :> ILazy<obj>)
+      (fun f -> LockFreeLazy f :> ILazy<obj>) ]
+    |> List.map (fun f -> TestCaseData(f))
 
-[<TestCaseSource("lazyImplementations")>]
-let ``Lazy implementation should return correct value`` (createLazyInstance: (unit -> int) -> ILazy<int>) =
-    let lazyValue = createLazyInstance testSupplier
-    let result1 = lazyValue.Get()
-    let result2 = lazyValue.Get()
-    Assert.AreEqual(1, result1)
-    Assert.AreEqual(1, result2)
+[<TestCaseSource(nameof(AllLazyCases))>]
+let ``Value should be computed only once`` (lazyConstructor: (unit -> obj) -> ILazy<obj>) =
+    let supplier () =
+        counter <- counter + 1
+        obj ()
 
-[<TestCaseSource("lazyImplementations")>]
-let ``Lazy implementation should compute value only once`` (createLazyInstance: (unit -> obj) -> ILazy<obj>) =
-    let counter = ref 0
-    let supplier = fun _ -> Interlocked.Increment(counter) |> ignore
-                            obj()
-    let lazyValue = createLazyInstance supplier
-    let result1 = lazyValue.Get()
-    let result2 = lazyValue.Get()
+    counter <- 0
+
+    let lazyObject = lazyConstructor supplier
+    let result1 = lazyObject.Get()
+    let result2 = lazyObject.Get()
+    
     result1 |> should equal result2
     counter |> should equal 1
 
+[<TestCaseSource(nameof(AllLazyCases))>]
+let ``Lazy should return the same value on each call`` (lazyConstructor: (unit -> obj) -> ILazy<obj>) =
+    let supplier () = obj ()
 
-[<TestCaseSource("lazyImplementations")>]
-let ``Lazy implementation should be thread-safe`` (createLazyInstance: (unit -> 'a) -> ILazy<'a>) =
-    let mutable computationCount = 0
-    let supplier = 
-        fun () -> 
-            computationCount <- computationCount + 1
-            Thread.Sleep(100)
-            1
-    let lazyValue = createLazyInstance supplier
-    let results = 
-        [for _ in 1..10 -> async { return lazyValue.Get() }]
-        |> Async.Parallel
-        |> Async.RunSynchronously
-    Assert.AreEqual(results |> Array.forall (fun v -> v = 1), true)
-    Assert.AreEqual(1, computationCount)
+    let lazyObject = lazyConstructor supplier
+    let result1 = lazyObject.Get()
+    let result2 = lazyObject.Get()
+
+    result1 |> should equal result2
+
+[<TestCaseSource(nameof(MultiThreadedLazyCases))>]
+let ``Lazy should return the same value in each thread`` (lazyConstructor: (unit -> obj) -> ILazy<obj>) =
+    let supplier () = obj ()
+
+    let lazyObject = MultiThreadedLazy supplier :> obj ILazy
+
+    Seq.initInfinite (fun _ -> async { return lazyObject.Get() })
+    |> Seq.take 8
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.distinct
+    |> Seq.length
+    |> should equal 1
+
+
+[<Test>]
+let ``Lazy should compute value only once`` () =
+    let counter = ref 0
+
+    let supplier () =
+        System.Threading.Interlocked.Increment(counter) |> ignore
+        obj ()
+
+    let lazyObject = MultiThreadedLazy supplier :> obj ILazy
+
+    Seq.initInfinite (fun _ -> async { return lazyObject.Get() })
+    |> Seq.take 12
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.distinct
+    |> Seq.length
+    |> should equal 1
+
+    counter.Value |> should equal 1
