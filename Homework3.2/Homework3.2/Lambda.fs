1dca1730b3d4491b6624e9d3042e6793d1373fd6
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-06-01 05:32:03 +0300

    added tests for lambda and added renaming variables

---
+++ Homework3.2/Homework3.2/Lambda.fs
@@ -0,0 +1,61 @@
+﻿module Lambda
+
+type LambdaExpression =
+    | Variable of string
+    | Abstraction of string * LambdaExpression
+    | Application of LambdaExpression * LambdaExpression
+
+let boundVar expr =
+    let rec boundVarAcc expr acc =
+        match expr with
+        | Variable _ -> acc
+        | Application(func, arg) -> boundVarAcc func acc @ boundVarAcc arg acc
+        | Abstraction(param, body) -> boundVarAcc body (param :: acc)
+    boundVarAcc expr [] |> List.distinct
+
+let freeVar expr =
+    let rec freeVarAcc expr acc =
+        match expr with
+        | Variable(x) -> x::acc
+        | Application(func, arg) -> freeVarAcc func acc @ freeVarAcc arg acc
+        | Abstraction(param, body) -> freeVarAcc body acc |> List.filter (fun x -> x <> param)
+    freeVarAcc expr [] |> List.distinct
+
+let newVar s t =
+    let rec newVarRec s t var =
+        if not (List.contains var (freeVar s @ freeVar t)) then
+            var
+        else
+            newVarRec s t (var + "'")
+    newVarRec s t "a"
+
+let rec alphaConvert var newVar expression =
+    match expression with
+    | Variable(x) when x = var -> Variable(newVar)
+    | Abstraction(param, body) when param = var -> Abstraction(newVar, alphaConvert var newVar body)
+    | Abstraction(param, body) -> Abstraction(param, alphaConvert var newVar body)
+    | Application(func, arg) -> Application(alphaConvert var newVar func, alphaConvert var newVar arg)
+    | _ -> expression
+
+let rec substitute expr varName subExpr = 
+    match expr with
+    | Variable name -> if name = varName then subExpr else expr
+    | Application (func, arg) -> 
+        Application (substitute func varName subExpr, substitute arg varName subExpr)
+    | Abstraction (param, body) when param = varName -> expr
+    | Abstraction (param, body) when not (List.contains varName (freeVar body)) || not (List.contains param (freeVar subExpr)) ->
+        Abstraction(param, substitute body varName subExpr)
+    | Abstraction(param, body) ->
+        let newParam = newVar body subExpr
+        Abstraction(newParam, substitute (substitute body param (Variable(newParam))) varName subExpr)
+
+let rec betaReduce expr =
+    match expr with
+    | Application (Abstraction(param, body), Application(func, arg)) -> 
+        Application ((substitute body param func), arg)
+    | Application (Abstraction (param, body), arg) -> 
+        betaReduce (substitute body param arg)
+    | Abstraction (param, body) -> 
+        Abstraction (param, betaReduce body)
+    | _ -> expr
+   
---
+++ Homework3.2/Homework3.2Test/LambdaTest.fs
@@ -0,0 +1,50 @@
+﻿module LambdaTest
+open Lambda
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Free variable of lambda(x).x y is "y"``() =
+    let inputExpr = Abstraction("x", Application(Variable "x", Variable "y"))
+    let freeVarExpr = freeVar inputExpr
+    let correctFreeVar = ["y"]
+    Assert.AreEqual(correctFreeVar, freeVarExpr)
+    
+[<Test>]
+let ``Bound variable of lambda(x).x y is "x"``() =
+    let inputExpr = Abstraction("x", Application(Variable "x", Variable "y"))
+    let boundVarExpr = boundVar inputExpr
+    let correctBoundVar = ["x"]
+    Assert.AreEqual(correctBoundVar, boundVarExpr)
+
+[<Test>]
+let ``(lambda(y).y x) z -> z x``() = 
+    let inputExpr = Application(Abstraction("y", Application(Variable "y", Variable "x")), Variable "z")
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Application(Variable "z", Variable "x")
+    Assert.AreEqual(correctExpr, reducedExpr)
+    
+let I = Abstraction("x", Variable "x")
+let K = Abstraction("x", Abstraction("y", Variable "x"))
+let S = Abstraction("x", Abstraction("y", Abstraction("z", Application(Application(Variable "x", Variable "z"), Application(Variable "y", Variable "z")))))
+
+let SKK = Application(Application(S, K), K)
+
+[<Test>]
+let ``S K K should be equivalent to I``() =
+    let reducedExpr = betaReduce SKK
+    Assert.AreEqual(I, reducedExpr)
+    
+[<Test>]
+let ``(lambda(y).y x) (lambda(z).z) -> x``() = 
+    let inputExpr = Application(Abstraction("y", Application(Variable "y", Variable "x")), Abstraction("z", Variable "z"))
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Variable "x"
+    Assert.AreEqual(correctExpr, reducedExpr)
+    
+[<Test>]
+let ``(lambda(x). x y) y -> y y``() =
+    let inputExpr = Application(Abstraction("x",Application(Variable "x", Variable "y")), Variable "y")
+    let reducedExpr = betaReduce inputExpr
+    let correctExpr = Application(Variable "y", Variable "y")
+    Assert.AreEqual(correctExpr, reducedExpr)
